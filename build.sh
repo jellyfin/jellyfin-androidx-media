9da45f2194720b6e146a2450ba3e5fde8c27ec7a
Niels van Velzen <nielsvanvelzen@users.noreply.github.com>
2023-04-27 13:34:10 +0200

    Enable FLAC as decoder (#105)

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ export ANDROID_NDK_PATH=$ANDROID_HOME/ndk/21.4.7075529
 export EXOPLAYER_ROOT="${PWD}/ExoPlayer"
 export FFMPEG_EXT_PATH="${EXOPLAYER_ROOT}/extensions/ffmpeg/src/main"
 export FFMPEG_PATH="${PWD}/ffmpeg"
-export ENABLED_DECODERS=(alac pcm_mulaw pcm_alaw mp3 aac ac3 eac3 dca mlp truehd)
+export ENABLED_DECODERS=(flac alac pcm_mulaw pcm_alaw mp3 aac ac3 eac3 dca mlp truehd)
 
 # Create softlink to ffmpeg
 ln -sf "${FFMPEG_PATH}" "${FFMPEG_EXT_PATH}/jni/ffmpeg"
